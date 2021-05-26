538ef92945cfafc998cc4d7774efb600de61211a
rbhamidi-ford <ramam.bhamidipati@gmail.com>
2021-05-26 14:46:53 +0000

    Configuration lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private final String welcomeMessage;
 
+    public WelcomeController(
+            @Value("${welcome.message}") String welcomeMessage
+    ) {
+        this.welcomeMessage = welcomeMessage;
+    }
 
-        @GetMapping("/")
-        public String sayHello() {
-            return "HELLO";
-
+    @GetMapping("/")
+    public String sayHello() {
+        return welcomeMessage;
     }
 }
